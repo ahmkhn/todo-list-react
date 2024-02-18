7119bd7ec3333f00d03d1a5fdca7c7d482467e10
Ahmed Khan <104601942+ahmkhn@users.noreply.github.com>
2024-02-17 19:17:18 -0500

    rendering lists through useState mapping

--- react-todo/src/Todo.jsx
+++ react-todo/src/Todo.jsx
@@ -1,14 +1,29 @@
 import { useState } from 'react';
 function Todo(){
+    const [taskArr,setTaskArr]=useState([]);
+    const [task,setTask]=useState("");
+    function handleAdd(){
+        const textTask = document.getElementById("enter-inp").value;
+        document.getElementById("enter-inp").value="";
+        setTask(t=>setTask(textTask));
+        setTaskArr(arr=>[...taskArr,textTask]);
+    }
     return(
         <>
             <div className="main-container">
-            <input type="text" placeholder="Enter a task" className="enter-input"></input>
-            <button className="add-button">+</button>
+            <input type="text" placeholder="Enter a task" className="enter-input" id="enter-inp"></input>
+            <button onClick={handleAdd} className="add-button">+</button>
             </div>
             <div className="second-container">
-                <div>
+                <div className="task-div">
                     <h1>Tasks</h1>
+                    <ul>
+                        {taskArr.map((element,index)=>
+                        <li className="list-elements" key={index}>
+                            {element}
+                            <button className="remove-button">X</button>
+                        </li>)}
+                        </ul>
                 </div>
             </div>
         </>
